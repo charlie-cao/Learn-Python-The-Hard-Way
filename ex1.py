4720a2f07ef685457e274cbd92c195dc1a6b5f6f
Charlie.Cao <964870916@qq.com>
2018-03-01 17:32:33 +0800

    three hour

---
+++ ex1.py
@@ -0,0 +1,22 @@
+#-- coding:utf-8 --
+
+print "Hello World!"
+
+print "Hello Again"
+
+print "I like typing this."
+
+print "This is fun"
+
+print "Yay! Printing."
+
+print "I'd much rather you"
+
+print 'I "asid" do not thouch this.'
+
+print "hello 来自中国"
+
+print "😊"
+
+
+#print "这应该是一个注释"
---
+++ ex23.py
@@ -0,0 +1,8 @@
+#-- coding:utf-8 --
+print """
+阅读网上的代码
+bitbucket.org 忽略 Python3
+github.com
+launchpad.net
+koders.com
+"""
